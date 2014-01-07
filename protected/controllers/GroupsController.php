41ddf196a8e6e49b73ddefa3f08ab389470b3abe
jaakko <jaakko@jaakko-Latitude-E6530>
2014-01-07 16:11:45 +0200

    Added a function to return the top tweets.

--- protected/controllers/GroupsController.php
+++ protected/controllers/GroupsController.php
@@ -3,7 +3,7 @@
 class GroupsController extends ApiController
 {
 
-    public function actionTweets($group, $from, $to)
+    public function actionTweetCounts($group, $from, $to)
     {
         $from = strtotime($from);
         $to = strtotime($to);
@@ -25,6 +25,38 @@ class GroupsController extends ApiController
         ));
         $tweets = $command->queryAll();
         $tweets = $this->valuesToJson($tweets, $from, $to, 'time', 'tweet_count');
+        $this->outputJson(array(
+            'group' => $group,
+            'tweets' => $tweets,
+        ));
+    }
+
+    public function actionTopTweets($group, $from, $to, $number)
+    {
+        $number = intval($number);
+        if(!$number) {
+           throw new CHttpException('Number has to be integer'); 
+        }
+        
+        $from = strtotime($from);
+        $to = strtotime($to);
+
+        date_default_timezone_set('UTC');
+
+        $sql = 'SELECT tweet.*, created_at + 2 * 60 * 60 AS time
+                FROM  `tweet` 
+                LEFT JOIN search_phrase ON search_phrase.id = tweet.search_phrase_id
+                LEFT JOIN  `group` ON group.id = search_phrase.group_id
+                WHERE  `group`.name = :group
+                AND tweet.created_at > :from
+                ORDER BY retweet_count + favorite_count DESC
+                LIMIT ' . $number;
+        $command = Yii::app()->db->createCommand($sql);
+        $command->execute(array(
+            ':group' => $group,
+            ':from' => $from,
+        ));
+        $tweets = $command->queryAll();
         $this->outputJson(array(
             'group' => $group,
             'tweets' => $tweets,
